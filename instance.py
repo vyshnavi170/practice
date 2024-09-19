57ffacccc018db4c5677ba205017ec5975060375
vyshnavi170 <vyshnavireddy37@gmail.com>
2024-09-19 22:13:42 +0530

    instance describe python boto3 code

---
+++ instance.py
@@ -0,0 +1,20 @@
+import boto3
+import sys
+
+region = sys.argv[1]
+acess_key = sys.argv[2]
+secret_key = sys.argv[3]
+
+client = boto3.client('ec2' region_name=region,aws_access_key_id=access_key,aws_secret_access_key=secret_key)
+response = client.describe_instances(
+    Filters=[
+        {
+            'Name': 'instance-type',
+            'Values': [
+                't2.small',
+            ],
+        },
+    ],
+)
+
+print(response)
